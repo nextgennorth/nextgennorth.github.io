9d2b225e9e9ef2b7edc7790b4e9f7f1183a6e0f9
Ehsan-saradar <Ehsan.Saradar@gmail.com>
2024-09-10 17:38:22 +0330

    Create index.ts

---
+++ app/copy/index.ts
@@ -0,0 +1,176 @@
+export const indexCopy = {
+    heroSection: {
+        tilte: "NextGenNorth",
+        slogan: `BIG IDEAS <br/> — BIGGER IMPACT`,
+        subtitle: "Northern Territory Stories That Inspire",
+        startButton: {
+            text: "START LISTENING",
+            url: "#"
+        },
+        description1: "We bring you the big ideas, and influential voices that are promoting the generational change of the Northern Territory.",
+        description2: "New Episodes Every Monday and Friday. Join us twice a week for valuable knowledge and perspectives that resonate with Territorians.",
+    },
+    top_episode: {
+        title: "TOP EPISODE FOR THIS WEEK",
+        items: [
+            {
+                title: "Next Gen North Episode #31",
+                creator: "Dr Sabtir Aulakh",
+                time: "54:00",
+                url: "#",
+                image: "./img/episods/episod1.png"
+            },
+            {
+                title: "Next Gen North Episode #30",
+                creator: "John Mitchell",
+                time: "1:12:41",
+                url: "#",
+                image: "./img/episods/episod2.png"
+            },
+            {
+                title: "Next Gen North Episode #29",
+                creator: "Greg Ireland",
+                time: "56:25",
+                url: "#",
+                image: "./img/episods/episod3.png"
+            },
+        ]
+    },
+    podcasts: {
+        title: "EXPLORE OUR PODCAST <br /> BY CATEGORIES",
+        categories: ["All", "Business", "Technology", "Health & Wellness", "Sustainability", "More..."],
+        contents: [
+            {
+                title: "Next Gen North Episode #31",
+                creator: "Dr Sabtir Aulakh",
+                time: "56:25",
+                Image: "./img/podcast/DrSatbirAulakh.png",
+                url: "#",
+                categories: ["All", "Business", "More..."],
+            },
+            {
+                title: "Next Gen North Episode #30",
+                creator: "John Mitchell",
+                time: "2:10",
+                Image: "./img/podcast/JohnMitchell.png",
+                categories: ["All", "Sustainability"],
+            },
+            {
+                title: "Next Gen North Episode #31",
+                creator: "Greg Ireland",
+                time: "1:30",
+                Image: "./img/podcast/GregIreland.png",
+                categories: ["All", "Technology", "More..."],
+            },
+            {
+                title: "Next Gen North Episode #31",
+                creator: "Christopher Walsh",
+                time: "20:56",
+                Image: "./img/podcast/ChristopherWalsh.png",
+                categories: ["All", "Health & Wellness"],
+            },
+            {
+                title: "Next Gen North Episode #31",
+                creator: "Andrew Ucles",
+                time: "11:11",
+                Image: "./img/podcast/AndrewUcles.png",
+                categories: ["All", "Sustainability"],
+            },
+            {
+                title: "Next Gen North Episode #31",
+                creator: "Minoli De Silva",
+                time: "2:11",
+                Image: "./img/podcast/MinoliDeSilva.png",
+                categories: ["All", "Health & Wellness", "More..."],
+            },
+            {
+                title: "Next Gen North Episode #31",
+                creator: "Dr Satbir Aulakh",
+                time: "2:10:25",
+                Image: "./img/podcast/DrSatbirAulakh.png",
+                categories: ["All", "Technology"],
+            },
+            {
+                title: "Next Gen North Episode #31",
+                creator: "Greg Ireland",
+                time: "25:25",
+                Image: "./img/podcast/GregIreland.png",
+                categories: ["All", "Technology"],
+            },
+            {
+                title: "Next Gen North Episode #31",
+                creator: "John Mitchell",
+                time: "1:56:25",
+                Image: "./img/podcast/JohnMitchell.png",
+                categories: ["All", "Business"],
+            }
+        ]
+    },
+    suported_by: {
+        title: "SUPORTED BY",
+        items: [
+            { image: "./img/suportedBy/spotify.png" },
+            { image: "./img/suportedBy/charles.png" },
+            { image: "./img/suportedBy/JPTHOR.png" },
+            { image: "./img/suportedBy/northern.png" },
+            { image: "./img/suportedBy/telstra.png" },
+            { image: "./img/suportedBy/spotify.png" },
+            { image: "./img/suportedBy/charles.png" },
+            { image: "./img/suportedBy/JPTHOR.png" },
+            { image: "./img/suportedBy/northern.png" },
+            { image: "./img/suportedBy/telstra.png" }
+        ]
+    },
+    news: {
+        title: "News",
+        items:
+            [{
+                title: "From Darwin to the World: Meet the Young Innovators Leading NT's Future",
+                writer: "Wade Warren",
+                date: "4/9/2024",
+                image: "./img/news/news1.png",
+                url: "#"
+            }, {
+                title: "How the Northern Territory is Pioneering Sustainability for Future Generations",
+                writer: "Wade Warren",
+                date: "4/9/2024",
+                image: "./img/news/news2.png",
+                url: "#"
+            }, {
+                title: "Tomorrow’s Leaders: Educational Paths Shaping the NT’s Next Generation",
+                writer: "Wade Warren",
+                date: "4/9/2024",
+                image: "./img/news/news3.png",
+                url: "#"
+            }, {
+                title: "From the Ground Up: Community Projects Empowering Youth in the NT",
+                writer: "Wade Warren",
+                date: "4/9/2024",
+                image: "./img/news/news4.png",
+                url: "#"
+            }]
+    },
+    PioneerNT: {
+        title: "UNLOCK THE FUTURE OF THE <br/>NORTHERN TERRITORY",
+        description_1: `Curious about how the Northern Territory is fostering the next generation of leaders, innovators, and changemakers? Our NextGen North Whitepaper dives deep into the vision, mission, and strategic goals behind the podcast that’s shaping the future of the NT. This comprehensive guide offers insights into the local entrepreneurial ecosystem, highlights key projects, and outlines how youth-driven innovation is creating lasting impact across the region.
+        <br>What You’ll Discover Inside:<br><br>`,
+
+        description_2: `<ul className="list-disc mt-4 ml-5 text-base">
+                            <li> Our mission to empower and inspire the next generation</li>
+                            <li>  Key interviews and success stories from local innovators</li>
+                            <li> Strategic partnerships and sponsorship opportunities</li>
+                            <li>  Ways to get involved and help shape the NT's future</li>
+                        </ul>`
+        ,
+        description_3: `Whether you're an entrepreneur, student, or community leader, the NextGen North Whitepaper offers valuable insights and actionable steps to drive positive change. Be part of the conversation, and learn how we can build a thriving, sustainable future for the Northern Territory together.`,
+
+        witepaper: "Download the Whitepaper Now",
+        description: "To access your free copy of the NextGen North Whitepaper, simply fill out the form below:",
+        full_name: "Full Name",
+        work_email: "Work Email",
+        phone_number: "Phone Number",
+        company: "Company/Organization (Optional)",
+        job_title: "Job Title",
+        button: { text: "Download New", url: "#" }
+    }
+}
